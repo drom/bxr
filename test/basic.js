0de4c409ee7485495d9f8edf0655fd4d445112a3
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-12-22 12:27:33 -0800

    dummy code, dummy test

--- test/basic.js
+++ test/basic.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const chai = require('chai');
+
+const lib = require('../lib/index.js');
+
+const expect = chai.expect;
+
+describe('basic', () => {
+  it('bxr is function', () => {
+    expect(lib.bx).to.be('function');
+  });
+});
+
+/* eslint-env mocha */
